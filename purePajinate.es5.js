14c3571a5ab6be73013e33456f015186ea00bc03
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2020-02-25 17:17:13 -0500

    Added the forEach polyfill
    Adds compatibility to all Browsers supporting ES5

--- purePajinate.es5.js
+++ purePajinate.es5.js
@@ -1,5 +1,14 @@
 "use strict";
 
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
 function _instanceof(left, right) {
   if (
     right != null &&
